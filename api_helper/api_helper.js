f0af380fc8f9a5c6f42782f1bcd137756afdea8a
tomaskate <workkateryna12370@gmail.com>
2023-09-13 10:45:18 +0300

    add try..catch

--- api_helper/api_helper.js
+++ api_helper/api_helper.js
@@ -1,18 +1,34 @@
 import fetch from "node-fetch";
 
 export async function getPosts(baseUrl) {
-  const response = await fetch(baseUrl);
-  const results = await response.json();
-  return results;
+  try {
+    const response = await fetch(baseUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status ${response.status}`);
+    }
+    const results = await response.json();
+    return results;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
 }
 
 export async function createPost(url, body) {
-  const response = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  });
-  return response.json();
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
 }
